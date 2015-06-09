5d4ef4b535cd05f0de9cfaff9559768ba1235dce
Luay Bseiso <luay@buttercloud.com>
2015-06-09 17:46:20 +0300

    add template functions for create-event and post to url

--- src/stripe_tester_clj/core.clj
+++ src/stripe_tester_clj/core.clj
@@ -1,7 +1,7 @@
 (ns stripe-tester-clj.core
   (:require [clojure.data.json :as json]))
 
-(def stripe-version "2015-04-07")
+(def latest-stripe-version "2015-04-07")
 
 (defn file-exists?
   [file-name]
@@ -13,6 +13,18 @@
      (if (file-exists? file-name)
        (json/read-str (slurp file-name) :key-fn keyword)
        (throw (Exception. "Invalid callback type")))))
+
   ([callback-type custom-data]
    (let [data (load-template callback-type)]
      (merge data custom-data))))
+
+(defn post-to-url
+  [data])
+
+(defn create-event
+  ([callback-type]
+   (let [data (load-template callback-type)]
+     (post-to-url data)))
+  ([callback-type custom-data]
+   (let [data (load-template callback-type custom-data)]
+     (post-to-url data))))

3f329296bdbd4b2c4ca47bfebf9195b4fb93bf99
Luay Bseiso <luay@buttercloud.com>
2015-06-09 14:22:22 +0300

    add load-template function that loads the json template with custom data as an option

--- src/stripe_tester_clj/core.clj
+++ src/stripe_tester_clj/core.clj
@@ -1,1 +1,18 @@
-(ns stripe-tester-clj.core)
+(ns stripe-tester-clj.core
+  (:require [clojure.data.json :as json]))
+
+(def stripe-version "2015-04-07")
+
+(defn file-exists?
+  [file-name]
+  (.exists (clojure.java.io/file file-name)))
+
+(defn load-template
+  ([callback-type]
+   (let [file-name (str "stripe-webhooks/" stripe-version "/" (name callback-type) ".json")]
+     (if (file-exists? file-name)
+       (json/read-str (slurp file-name) :key-fn keyword)
+       (throw (Exception. "Invalid callback type")))))
+  ([callback-type custom-data]
+   (let [data (load-template callback-type)]
+     (merge data custom-data))))
--- test/stripe_tester_clj/core_test.clj
+++ test/stripe_tester_clj/core_test.clj
@@ -2,6 +2,30 @@
   (:require [clojure.test :refer :all]
             [stripe-tester-clj.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(testing "Sending Data"
+  (testing "Send data to webhook URL"))
+
+(testing "Importing Data"
+  (testing "Import webhook JSON data to clojure data"
+    (testing "Default data"
+      (deftest correct-data-type
+        (let [data (load-template :invoice-created)]
+          (is (instance? clojure.lang.PersistentHashMap data))))
+      (deftest correct-callback-type-inside-webhook
+        (let [callback-type :invoice-created
+              data (load-template callback-type)]
+          (is (= callback-type (keyword (clojure.string/replace (:type data) #"\." "-"))))))
+      (deftest raise-exception-for-invalid-callback-type
+        (is (thrown-with-msg? Exception #"Invalid callback type" (load-template :invalid-callback)))))
+    (testing "With custom data"
+      (testing "Top level"
+        (deftest merge-top-level-custom-data
+          (let [custom-data {:id "new_id_111"}
+                data (load-template :invoice-created custom-data)]
+            (is (= (:id data) (:id custom-data))))))
+      (testing "Deep nested level"
+        (deftest merge-deep-level-custom-data
+          (let [custom-data {:data {:object {:lines {:data {:plan {:interval "xxx"}}}}}}
+                data (load-template :invoice-created custom-data)
+                keys [:data :object :lines :data :plan :interval]]
+            (is (= (get-in data keys) (get-in custom-data keys)))))))))
